ed25a84cfd6721d9d43907603e48f49b6f1c577e
Filipe Bacof <filipebacof@gmail.com>
2023-04-24 15:58:27 -0300

    reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <nav id="navbar">
-        <h2>
-          <Link to="/">MoviesLib</Link>
-        </h2>
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+      <Navbar />
       <h2>Movies Lib</h2>
+      <Outlet />
     </div>
   )
 }
